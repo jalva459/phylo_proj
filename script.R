be1f7841785f3e51649a0e40dbd8c6997ccb8804
jalva459 <jalvarez062001@gmail.com>
2026-06-26 10:02:22 -0500

    Initial commit for phylo proj working script

--- script.R
+++ script.R
@@ -1,5 +1,6 @@
 #pipe into github repo
 library(usethis)
+use_git_config(user.name="jalva459", user.email="jalvarez062001@gmail.com")
 use_git()
 
 #install packages and load libraries that you will work with 

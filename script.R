631ef6d2b339d51bfdcbebaf612585b5b1098b2d
jalva459 <jalvarez062001@gmail.com>
2026-06-26 09:37:12 -0500

    Initial commit

---
+++ script.R
@@ -0,0 +1,70 @@
+#install packages and load libraries that you will work with 
+install.packages(picante)
+library(picante)
+#load phylogenetic trees from Desktop
+# import abundance data from Murphy paper
+# Remember that we have three objects containing the 
+# community (comm)
+# phylogeny (phy), 
+# and trait (traits) data sets.
+
+
+
+
+### I need to conduct the following calculations for each of the trees 
+### generated: the complete island phylogeny and the individual ones for 
+### each habitat type
+
+
+
+
+
+# Habitat Type: Complete 50-Ha Plot (WP=Whole Plot)
+#calculate Phylogenetic diversity (PD)
+  # WP.pd.result <- pd(comm, phy, include.root=TRUE)
+  # WP.pd.result
+
+
+#calculate standardized effect size (SES)
+#calculate Mean Phylogenetic Distance (MPD)
+#calculate Mean Nearest Taxon Distance (MNTD)
+#calculate Net relatedness Index (NRI)
+
+# Habitat Type: Swamp (SW)
+#calculate Phylogenetic diversity (PD)
+  # SW.pd.result <- pd(comm, phy, include.root=TRUE)
+  # SW.pd.result
+#calculate standardized effect size (SES)
+#calculate Mean Phylogenetic Distance (MPD)
+#calculate Mean Nearest Taxon Distance (MNTD)
+#calculate Net relatedness Index (NRI)
+
+# Habitat Type: High Plateau (HP)
+#calculate Phylogenetic diversity (PD)
+  # HP.pd.result <- pd(comm, phy, include.root=TRUE)
+  # HP.pd.result
+#calculate standardized effect size (SES)
+#calculate Mean Phylogenetic Distance (MPD)
+#calculate Mean Nearest Taxon Distance (MNTD)
+#calculate Net relatedness Index (NRI)
+
+# Habitat Type: Low-Plateau (LP)
+#calculate Phylogenetic diversity (PD)
+  # LP.pd.result <- pd(comm, phy, include.root=TRUE)
+  # LP.pd.result
+
+#calculate standardized effect size (SES)
+#calculate Mean Phylogenetic Distance (MPD)
+#calculate Mean Nearest Taxon Distance (MNTD)
+#calculate Net relatedness Index (NRI)
+
+# Habitat Type: Slope (SL)
+#calculate Phylogenetic diversity (PD)
+  # SL.pd.result <- pd(comm, phy, include.root=TRUE)
+  # SL.pd.result
+
+#calculate standardized effect size (SES)
+#calculate Mean Phylogenetic Distance (MPD)
+#calculate Mean Nearest Taxon Distance (MNTD)
+#calculate Net relatedness Index (NRI)
+
